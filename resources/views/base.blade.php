cb3c75e8492fa407bd4de7fd2db697c454ec5438
Aldi Unanto <aldiunanto@yahoo.com>
2017-05-24 15:15:46 +0700

    Loaded jquery.gmap

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -189,5 +189,38 @@
 	<script type="text/javascript" src="{{ asset('js/jquery.js') }}"></script>
 	<script type="text/javascript" src="{{ asset('js/plugins.js') }}"></script>
 	<script type="text/javascript" src="{{ asset('js/functions.js') }}"></script>
+	<script type="text/javascript" src="https://maps.google.com/maps/api/js"></script>
+	<script type="text/javascript" src="js/jquery.gmap.js"></script>
+	<script type="text/javascript">
+		$(function(){
+			if($('#google-map').length > 0){
+				jQuery('#google-map').gMap({
+					address: 'Melbourne, Australia',
+					maptype: 'ROADMAP',
+					zoom: 14,
+					markers: [
+						{
+							address: "Melbourne, Australia",
+							html: '<div style="width: 300px;"><h4 style="margin-bottom: 8px;">Hi, we\'re <span>Envato</span></h4><p class="nobottommargin">Our mission is to help people to <strong>earn</strong> and to <strong>learn</strong> online. We operate <strong>marketplaces</strong> where hundreds of thousands of people buy and sell digital goods every day, and a network of educational blogs where millions learn <strong>creative skills</strong>.</p></div>',
+							icon: {
+								image: "images/icons/map-icon-red.png",
+								iconsize: [32, 39],
+								iconanchor: [32,39]
+							}
+						}
+					],
+					doubleclickzoom: false,
+					controls: {
+						panControl: true,
+						zoomControl: true,
+						mapTypeControl: true,
+						scaleControl: false,
+						streetViewControl: false,
+						overviewMapControl: false
+					}
+				});
+			}
+		});
+	</script>
 </body>
 </html>
